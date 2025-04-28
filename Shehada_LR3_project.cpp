ea8891f870c67b4fa12b7a872a23827da684a4a5
shehadaa <usameelfrhancom@gmail.com>
2025-04-29 00:29:42 +0300

    create void main

---
+++ Shehada_LR3_project.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+// Функция для ввода символа
+char inputChar() {
+
+}
+
+// Функция для вывода предыдущего символа в ASCII
+void printPreviousChar(char ch) {
+
+}
+
+// Функция для вывода следующего символа в ASCII
+void printNextChar(char ch) {
+
+}
+
+// Функция для отображения меню
+void displayMenu() {
+    cout << "\nМеню:\n";
+    cout << "1. Ввести символ и показать предыдущий\n";
+    cout << "2. Ввести символ и показать следующий\n";
+    cout << "3. Выход\n";
+    cout << "Выберите действие: ";
+}
+
+int main() {
+    int choice;
+    
+    do {
+        displayMenu();
+        cin >> choice;
+        
+        switch(choice) {
+            case 1: {
+                char ch = inputChar();
+                printPreviousChar(ch);
+                break;
+            }
+            case 2: {
+                char ch = inputChar();
+                printNextChar(ch);
+                break;
+            }
+            case 3:
+                cout << "Выход из программы.\n";
+                break;
+            default:
+                cout << "Неверный выбор. Попробуйте снова.\n";
+        }
+    } while(choice != 3);
+    
+    return 0;
+}
